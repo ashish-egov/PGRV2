f59d9a64fbae4c1f5f4954537e968e578358defb
ashish-egov <ashish.tiwari@egovernments.org>
2024-11-18 19:08:24 +0530

    Added Comments

--- src/main/java/digit/web/controllers/RequestApiController.java
+++ src/main/java/digit/web/controllers/RequestApiController.java
@@ -1,41 +1,21 @@
 package digit.web.controllers;
 
-import digit.config.PGRConstants;
-import digit.service.EnrichmentService;
 import digit.service.PgrService;
-import digit.service.WorkflowService;
-import digit.validator.PgrValidator;
-import digit.web.models.ErrorRes;
 import digit.web.models.RequestSearchCriteria;
 import digit.web.models.ServiceRequest;
 import digit.web.models.ServiceResponse;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.enums.ParameterIn;
-import io.swagger.v3.oas.annotations.media.Schema;
 
 import org.egov.common.contract.models.RequestInfoWrapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.bind.annotation.RequestMapping;
 import java.io.IOException;
-import java.util.*;
-
-import jakarta.validation.constraints.*;
 import jakarta.validation.Valid;
-import jakarta.servlet.http.HttpServletRequest;
 
 @jakarta.annotation.Generated(value = "org.egov.codegen.SpringBootCodegen", date = "2024-11-18T11:00:08.741990949+05:30[Asia/Kolkata]")
 @Controller
@@ -43,27 +23,38 @@ import jakarta.servlet.http.HttpServletRequest;
 public class RequestApiController {
 
     @Autowired
-    private ObjectMapper objectMapper;
-
-    @Autowired
-    private HttpServletRequest request;
-
-    @Autowired
     private PgrService pgrService;
 
+    /**
+     * Creates a new service request and returns the response.
+     *
+     * @param requestBody The service request details provided in the request body.
+     * @return The created service response wrapped in a ResponseEntity.
+     * @throws IOException If an I/O error occurs during processing.
+     */
     @RequestMapping(value = "/_create", method = RequestMethod.POST)
     public ResponseEntity<ServiceResponse> requestsCreatePost(@Valid @RequestBody ServiceRequest requestBody)
             throws IOException {
 
         ServiceResponse response = pgrService.create(requestBody);
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(response); // Return the created response
     }
 
+    /**
+     * Searches for service requests based on provided criteria.
+     *
+     * @param requestInfoWrapper The request information wrapper containing request
+     *                           details.
+     * @param criteria           The search criteria for the request.
+     * @return The service response wrapped in a ResponseEntity (currently not
+     *         implemented).
+     */
     @RequestMapping(value = "/_search", method = RequestMethod.POST)
     public ResponseEntity<ServiceResponse> requestsSearchPost(@Valid @RequestBody RequestInfoWrapper requestInfoWrapper,
             @Valid @ModelAttribute RequestSearchCriteria criteria) {
 
         ServiceResponse response = pgrService.search(requestInfoWrapper.getRequestInfo(), criteria);
-        return new ResponseEntity<ServiceResponse>(HttpStatus.NOT_IMPLEMENTED);
+        return new ResponseEntity<ServiceResponse>(HttpStatus.NOT_IMPLEMENTED); // Return not implemented status
     }
+
 }
